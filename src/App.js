d00f230c0276baa823d803fba80f42511e7b43e1
Marouane <marouane.abra@gmail.com>
2019-12-06 16:12:26 -0500

    improving website

--- src/App.js
+++ src/App.js
@@ -5,28 +5,27 @@ import './App.css';
 class App extends React.Component {
   render() {
     return   (<div class="fullscreen-bg">
-    <video loop muted autoplay poster="img/aurora_lights_iceland_small.jpg" class="fullscreen-bg_video">
-        <source src="video/aurora_lights_iceland.mp4" type="video/mp4">
-    </video>
-  </div>
+    <video loop muted autoplay poster="aurora_lights_iceland_small.jpg" class="fullscreen-bg_video">
+        <source src="aurora_lights_iceland.mp4" type="video/mp4"/>
 
-  <!-- Centered div -->
+    </video>
+
+
   <div id="center_div_homepage">
+
 
     <h1> Welcome to Aurora Finder </h1>
 
     <p>Please input either a date of when you
       want to see an aurora or a location</p>
 
-    <!-- Location input form -->
-    <form id="location" action:"/dummy-server">
-      <input type="text" placeholder="Location">
+    <form id="location">
+      <input type="text" placeholder="Location"/>
       <button type="submit">Submit</button>
     </form>
 
-    <!-- Date input form -->
-    <form id="date" action:"/dummy-server">
-      <input type="text" placeholder="Date">
+    <form id="date">
+      <input type="text" placeholder="Date"/>
       <button type="submit">Submit</button>
     </form>
 
@@ -35,7 +34,7 @@ class App extends React.Component {
       <p>Live preview of the next 30 minutes of the Aurora Borealis</p>
     </a>
 
-  <!-- End centered div -->
+  </div>
   </div>);
   }
 }
